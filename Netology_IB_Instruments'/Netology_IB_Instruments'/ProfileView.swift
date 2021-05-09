452b6880ab0ee7a9d054c0e6aa7309a6f3818f87
AndrewJackson22 <mihailov.911@list.ru>
2021-05-09 19:42:22 +0300

    Add new controller

--- Netology_IB_Instruments'/Netology_IB_Instruments'/ProfileView.swift
+++ Netology_IB_Instruments'/Netology_IB_Instruments'/ProfileView.swift
@@ -9,4 +9,11 @@ import UIKit
 
 class ProfileView: UIView {
     
+    @IBOutlet weak var photoImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var birthdayLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var textLabel: UITextView!
+    
+    
 }
---
+++ Netology_IB_Instruments'/Netology_IB_Instruments'/ProfileViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ProfileViewController.swift
+//  Netology_IB_Instruments'
+//
+//  Created by Андрей Михайлов on 09.05.2021.
+//
+
+import UIKit
+
+
+class ProfileViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
